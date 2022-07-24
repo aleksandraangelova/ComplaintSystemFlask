3b610f3944e93ac1a212da7efe6873017e3bf1aa
Aleksandra Angelova <aleksandra.angelova2@gmail.com>
2022-07-24 21:54:35 +0300

    Create models for complaint system

---
+++ models/complaint.py
@@ -0,0 +1,17 @@
+from sqlalchemy import func
+
+from db import db
+from models.enums import ComplaintState
+
+
+class ComplaintModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    photo_url = db.Column(db.String(255), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    created_on = db.Column(db.Datetime, nullable=False, server_default=func.now())
+    status = db.Column(db.Enum(), nullable=False, default=ComplaintState.pending)
+    complainer_id = db.Column(db.Integer(), db.ForeignKey("complainers.id"), nullable=False)
+    complainer = db.relationship("ComplainerModel")
+
