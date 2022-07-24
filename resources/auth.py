09ad60be4327d458c6dcfae74bc365c570b005c4
Aleksandra Angelova <aleksandra.angelova2@gmail.com>
2022-07-24 22:22:30 +0300

    Add resources

---
+++ resources/auth.py
@@ -0,0 +1,14 @@
+from flask import request
+from flask_restful import Resource
+
+from db import db
+from models.user import ComplainerModel
+
+
+class RegisterResource(Resource):
+    def post(self):
+        data = request.get_json()
+        user = ComplainerModel(**data)
+        db.session.add(user)
+        db.session.commit()
+        return 201
