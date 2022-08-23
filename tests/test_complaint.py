5a0ed52f4decb2cd30adfa015dbd4fbb1a0f7509
Aleksandra Angelova <aleksandra.angelova2@gmail.com>
2022-08-23 21:13:56 +0300

    Finish testing happy path for complaint creation

--- tests/test_complaint.py
+++ tests/test_complaint.py
@@ -3,14 +3,19 @@ from unittest.mock import patch
 
 from flask_testing import TestCase
 
+from config import create_app
 from constants.common import TEMP_DIR
 from db import db
-from config import create_app
 from managers.complaint import ComplaintManager
-from models import ComplaintState
+from models import Complaint, ComplaintState, TransactionModel
 from services.s3 import S3Service
 from tests.factories import ComplainerFactory
-from tests.helpers import generate_token, encoded_photo, encoded_photo_extension, mock_uuid
+from tests.helpers import (
+    encoded_photo,
+    encoded_photo_extension,
+    generate_token,
+    mock_uuid,
+)
 
 
 class TestComplaint(TestCase):
@@ -31,20 +36,33 @@ class TestComplaint(TestCase):
         # TODO
         pass
 
-    @patch.object(ComplaintManager, "issue_transaction", return_value={
-        "quote_id": "11-22",
-        "recipient_id": "11",
-        "transfer_id": "012",
-        "target_account_id": "321",
-        "amount": 10,
-        "complaint_id": "1",
-    })
+    @patch.object(
+        ComplaintManager,
+        "issue_transaction",
+        return_value={
+            "quote_id": "11-22",
+            "recipient_id": "11",
+            "transfer_id": "012",
+            "target_account_id": "321",
+            "amount": 10,
+            "complaint_id": 1,
+        },
+    )
     @patch("uuid.uuid4", mock_uuid)
     @patch.object(S3Service, "upload_photo", return_value="some.s3.url")
     def test_create_complaint(self, mocked_s3, mocked_transaction):
+        complaints = Complaint.query.all()
+        assert len(complaints) == 0
+
+        transactions = TransactionModel.query.all()
+        assert len(transactions) == 0
+
         user = ComplainerFactory()
         token = generate_token(user)
-        headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+        }
         data = {
             "title": "Test Title",
             "description": "Test Description",
@@ -62,9 +80,27 @@ class TestComplaint(TestCase):
             "photo_url": mocked_s3.return_value,
             "amount": data["amount"],
             "id": resp["id"],
-            "title": data["title"]
+            "title": data["title"],
         }
         assert resp == expected_resp
+
         file_name = f"{str(mock_uuid())}.{encoded_photo_extension}"
         path = os.path.join(TEMP_DIR, file_name)
+
         mocked_s3.assert_called_once_with(path, file_name)
+        mocked_transaction.assert_called_once_with(
+            data["amount"],
+            f"{user.first_name} {user.last_name}",
+            user.iban,
+            mocked_transaction.return_value["complaint_id"]
+        )
+
+        # Test DB
+        complaints = Complaint.query.all()
+        assert len(complaints) == 1
+
+        # # Can build what we expect in the record
+        # assert dict(complaints[0]) == {""}
+
+        transactions = TransactionModel.query.all()
+        assert len(transactions) == 1
